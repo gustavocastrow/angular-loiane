0ca2ff2b14d65186f3241aba395699e2a9db03c9
Gustavo Castro <gustavocastrocs@gmail.com>
2023-01-04 22:56:36 -0300

    #03 - Configurando Ambiente

---
+++ primeiro-projeto/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        MeuPrimeiroComponent,
+        MeuPrimeiro2Component
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+    ]
+})
+export class AppModule { }
---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component', //tag html que vai renderizar o component
+  templateUrl: './meu-primeiro.component.html'
+  })
+
+  export class MeuPrimeiroComponent{
+
+  }
